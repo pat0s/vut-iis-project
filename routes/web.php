88515c22c0183f9bf16a5a47c02941dda0b2a9b4
freadrix <anton.medvedev.1423@gmail.com>
2022-10-30 15:31:10 +0100

    feat: create basic routes and views

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,78 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Main page
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function() {
-	return 'Hello world!';
-}
+// --------------------- User ---------------------------
+// Show user registration form
+Route::get('/registration', function () {
+    return view('welcome');
+});
 
-);
+// Show user sign-in form
+Route::get('/sign-in', function () {
+    return view('welcome');
+});
+
+// Log user out
+Route::post('/logout', function () {
+    return view('welcome');
+});
+
+// Create new user
+Route::post('/users', function () {
+    return view('welcome');
+});
+
+// Log user in
+Route::post('/users/authenticate', function () {
+    return view('welcome');
+});
+
+// Single user
+Route::get('/users/{user_id}', function () {
+    return view('welcome');
+});
+
+
+Route::get('/user/{user_id}/edit', function () {
+    return view('welcome');
+});
+// -----------------------------------------------------
+
+// --------------------- Team ---------------------------
+
+Route::get('/teams/{team_id}', function () {
+    return view('welcome');
+});
+
+Route::get('/teams/create', function () {
+    return view('welcome');
+});
+
+// -----------------------------------------------------
+
+Route::get('/create-tournament', function () {
+    return view('welcome');
+});
+
+
+Route::get('/tournament/{tournament_id}/edit', function () {
+    return view('welcome');
+});
+
+Route::get('/team/{team_id}/edit', function () {
+    return view('welcome');
+});
+
+Route::get('/statistics', function () {
+    return view('welcome');
+});
+
+Route::get("/team/{team_id}", function($team_id){
+    dd($team_id);
+    return response("Post " . $team_id);
+})->where('team_id', '[0-9]+');
