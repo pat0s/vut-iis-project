723f430a9a901f6c8b90b6f11fd82b64f12f62ac
Dalibor Králik <dadokralik@gmail.com>
2022-11-16 17:40:19 +0100

    feat: Create view for "Chagne password subpage"

--- resources/views/user/password.blade.php
+++ resources/views/user/password.blade.php
@@ -1,38 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{asset('css/style.css')}}">
-    <title>Duck Tournaments</title>
-</head>
-<body>
-<main id="">
-    <section>
-        <h1>Change password</h1>
+<x-layout>
 
-            <form method="POST" action="/password/update">
-                @csrf
+    <main id="change-password-page">
+        <h2>Change password</h2>
+        <section>
 
-                <input type="password" placeholder="Old password*" name="old_password"/>
-                @error('old_password')
-                    <p style="color:red;">{{$message}}</p>
-                @enderror
-                <input type="password" placeholder="New password*" name="new_password"/>
-                @error('new_password')
-                    <p style="color:red;">{{$message}}</p>
-                @enderror
-                <input type="password" placeholder="Repeat new password*" name="new_password_confirmation"/>
-                @error('new_password_confirmation')
-                    <p style="color:red;">{{$message}}</p>
-                @enderror
+                <form method="POST" action="/password/update">
+                    @csrf
 
-                <input type="submit" value="Change password" name="btn">
-            </form>
+                    <input type="password" placeholder="Old password*" name="old_password"/>
+                    @error('old_password')
+                        <p style="color:red;">{{$message}}</p>
+                    @enderror
+                    <input type="password" placeholder="New password*" name="new_password"/>
+                    @error('new_password')
+                        <p style="color:red;">{{$message}}</p>
+                    @enderror
+                    <input type="password" placeholder="Repeat new password*" name="new_password_confirmation"/>
+                    @error('new_password_confirmation')
+                        <p style="color:red;">{{$message}}</p>
+                    @enderror
 
-    </section>
-</main>
+                    <div id="form-buttons">
+                        <button type="submit" name="submit-button">Change password</button>
+                        <a href="/user/{{auth()->user()->person_id}}" class="button-styled">Cancel</a>
+                    </div>
+                </form>
 
-</body>
-</html>
+        </section>
+    </main>
+
+
+</x-layout>
