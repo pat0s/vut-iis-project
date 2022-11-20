b695aa8b99ecd561e43986f04eb3f2dcb4dcecfb
Dalibor Králik <dadokralik@gmail.com>
2022-11-20 17:51:26 +0100

    feat: add time function on flash message

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,3 +1,3 @@
-<div id="flash-message" class="{{$successOrerror}}">
+<div x-data="{show: true}" x-init="setTimeout(() => show = false, 2000)" x-show="show" id="flash-message" class="{{$successOrerror}}">
     <h2>{{$message}}</h2>
 </div>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <script src="//unpkg.com/alpinejs" defer></script>
     <script defer src="{{asset('js/app.js')}}"></script>
     <title>Duck Tournaments</title>
 </head>
