69eb745a067fa82a9cd7abf997c6e060860e0743
Dalibor Králik <dadokralik@gmail.com>
2022-11-20 13:26:16 +0100

    fix: add auth adn guest condition in USERS list

--- resources/views/components/user-page/users-list-item.blade.php
+++ resources/views/components/user-page/users-list-item.blade.php
@@ -1,5 +1,11 @@
 <li class="user-list-item">
-    <a href="/users/{{$userID}}" {{$userID == auth()->user()->person_id ? 'id=highlighted-user' :""}}>
+    @auth
+        <a href="/users/{{$userID}}" {{$userID == auth()->user()->person_id ? 'id=highlighted-user' :""}}>
+    @endauth
+    
+    @guest
+        <a href="/users/{{$userID}}">
+    @endguest
         <p>{{$userID}}</p>
         <p>{{$username}}</p>
         <p>{{$firstName}}</p>
