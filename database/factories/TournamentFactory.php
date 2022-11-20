94360f090895d37f5c708074c66d37eefa8beb9d
Patrik Sehnoutek <patriksehnoutek@gmail.com>
2022-11-20 16:15:01 +0100

    feat: add tournament end date to migration

--- database/factories/TournamentFactory.php
+++ database/factories/TournamentFactory.php
@@ -14,11 +14,14 @@ class TournamentFactory extends Factory
     public function definition()
     {
         $tournamentNames = ['World Championship', 'Spring tournament', 'Winter tournament'];
+        $startDate = $this->faker->dateTimeBetween('+0 days', '+1 month');
+        $startDateClone = clone $startDate;
 
         return [
             'tournament_name' => $this->faker->randomElement($tournamentNames),
             'description' => $this->faker->paragraph(),
-            'start_date' => $this->faker->dateTime(),
+            'start_date' => $startDate,
+            'end_date' => $this->faker->dateTimeBetween($startDate, $startDateClone->modify('+10 days')),
             'pricepool' => $this->faker->randomFloat(2, 1000, 100000),
             'is_approved' => rand(0,1),
             'number_of_participants' => $this->faker->randomElement(array(4,8,16,32,64)),
