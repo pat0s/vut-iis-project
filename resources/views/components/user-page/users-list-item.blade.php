52d64fcb824bf8cc914f2af1d6ebac3df6015b00
Dalibor Králik <dadokralik@gmail.com>
2022-11-20 13:18:47 +0100

    feat: Highlight logged user in USERS list

--- resources/views/components/user-page/users-list-item.blade.php
+++ resources/views/components/user-page/users-list-item.blade.php
@@ -1,8 +1,9 @@
 <li class="user-list-item">
-    <a href="/users/{{$userID}}">
+    <a href="/users/{{$userID}}" {{$userID == auth()->user()->person_id ? 'id=highlighted-user' :""}}>
         <p>{{$userID}}</p>
         <p>{{$username}}</p>
         <p>{{$firstName}}</p>
         <p>{{$surname}}</p>
     </a>
 </li>
+
