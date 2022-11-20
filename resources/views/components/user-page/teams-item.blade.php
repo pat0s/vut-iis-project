2d9c7fc977c4adb5418155db54c70a02c7276804
Dalibor Králik <dadokralik@gmail.com>
2022-11-20 13:57:00 +0100

    fix: Fix and add correct icons to profile TEAMS section and to team page

--- resources/views/components/user-page/teams-item.blade.php
+++ resources/views/components/user-page/teams-item.blade.php
@@ -3,7 +3,7 @@
     <a href="/teams/{{$team->team_id}}">
         {{$team->team_name}}
     </a>
-    @if($profileOwner and $team->manager_id == auth()->user()->person_id)
-        <a href="/teams/{{$team->team_id}}/delete"><img src="{{asset('img/Trash.svg')}}" alt="Trash"></a>
+    @if($profileOwner and $team->manager_id != auth()->user()->person_id)
+        <a href="/teams/{{$team->team_id}}/delete"><img src="{{asset('img/exit.svg')}}" alt="Leave"></a>
     @endif
 </li>
