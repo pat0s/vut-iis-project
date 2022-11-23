aed501ea81d96ff4fde60b2ddc95f7b67894f4ad
Dalibor Králik <dadokralik@gmail.com>
2022-11-23 15:05:45 +0100

    fix: Fix CSS in TOURNAMENT CREATE page

--- resources/views/tournaments/create.blade.php
+++ resources/views/tournaments/create.blade.php
@@ -7,63 +7,49 @@
             <form method="POST" action="/tournaments">
                 @csrf
 
-                <div>
-                    <label class="item" for="tournament_name">Tournament name</label>
-                    <input class="item" type="text" name="tournament_name">
+                <div class="item">
+                    <label for="tournament_name">Tournament name</label>
+                    <input type="text" name="tournament_name">
                     @error('tournament_name')
                         <p style="color:red;">{{$message}}</p>
                     @enderror
                 </div>
-{{--                <label class="item" for="tournament-name">Tournament name</label>--}}
-{{--                <input class="item" type="text" name="tournament_name">--}}
-{{--                @error('tournament_name')--}}
-{{--                    <p style="color:red;">Penis</p>--}}
-{{--                @enderror--}}
 
-                <div>
-                    <label class="item" for="tournament-description">Description</label>
-                    <textarea class="item" name="description" id="tournament-description" cols="50" rows="5"></textarea>
+                <div class="item">
+                    <label for="tournament-description">Description</label>
+                    <textarea name="description" id="tournament-description" cols="50" rows="5"></textarea>
                     @error('description')
                         <p style="color:red;">{{$message}}</p>
                     @enderror
                 </div>
 
-{{--                <label class="item" for="tournament-description">Description</label>--}}
-{{--                <textarea class="item" name="tournament_description" id="tournament-description" cols="50" rows="5"></textarea>--}}
-
-                <div>
-                    <label class="item" for="tournament-start-date">Start date</label>
-                    <input class="item" type="datetime-local" name="start_date" id="tournament-start-date">
+                <div class="item">
+                    <label for="tournament-start-date">Start date</label>
+                    <input type="datetime-local" name="start_date" id="tournament-start-date">
                     @error('start_date')
                         <p style="color:red;">{{$message}}</p>
                     @enderror
                 </div>
-{{--                <label class="item" for="tournament-start-date">Start date</label>--}}
-{{--                <input class="item" type="datetime-local" name="tournament_start_date" id="tournament-start-date">--}}
 
-                <div>
-                    <label class="item" for="tournament-end-date">End date</label>
-                    <input class="item" type="datetime-local" name="end_date" id="tournament-end-date">
+                <div class="item">
+                    <label for="tournament-end-date">End date</label>
+                    <input type="datetime-local" name="end_date" id="tournament-end-date">
                     @error('end_date')
                         <p style="color:red;">{{$message}}</p>
                     @enderror
                 </div>
-{{--                <label class="item" for="tournament-end-date">End date</label>--}}
-{{--                <input class="item" type="datetime-local" name="tournament_end_date" id="tournament-end-date">--}}
 
-                <div>
-                    <label class="item" for="tournament-pricepool">Pricepool</label>
-                    <input class="item" type="text" name="pricepool" id="tournament-pricepool">
+                <div class="item">
+                    <label for="tournament-pricepool">Pricepool</label>
+                    <input type="text" name="pricepool" id="tournament-pricepool">
                     @error('pricepool')
                         <p style="color:red;">{{$message}}</p>
                     @enderror
                 </div>
-{{--                <label class="item" for="tournament-pricepool">Pricepool</label>--}}
-{{--                <input class="item" type="text" name="tournament_pricepool" id="tournament-pricepool">--}}
 
-                <div>
-                    <label class="item" for="tournament-number-of-participants">Number of participants</label>
-                    <select class="item" id="tournament-number-of-participants" name="number_of_participants">
+                <div class="item">
+                    <label for="tournament-number-of-participants">Number of participants</label>
+                    <select id="tournament-number-of-participants" name="number_of_participants">
                         <option value="64">64</option>
                         <option value="32">32</option>
                         <option value="16">16</option>
@@ -75,19 +61,10 @@
                         <p style="color:red;">{{$message}}</p>
                     @enderror
                 </div>
-{{--                <label class="item" for="tournament-number-of-participants">Number of participants</label>--}}
-{{--                <select class="item" id="tournament-number-of-participants" name="tournament_number_of_participants">--}}
-{{--                    <option value="64">64</option>--}}
-{{--                    <option value="32">32</option>--}}
-{{--                    <option value="16">16</option>--}}
-{{--                    <option value="8">8</option>--}}
-{{--                    <option value="4">4</option>--}}
-{{--                    <option value="2">2</option>--}}
-{{--                </select>--}}
 
-                <div>
-                    <label class="item" for="tournament-sport">Sport</label>
-                    <select class="item" id="tournament-sport" name="sport_id">
+                <div class="item">
+                    <label for="tournament-sport">Sport</label>
+                    <select id="tournament-sport" name="sport_id">
                         @foreach($sports as $sport)
                             <option value="{{$sport->sport_id}}">{{$sport->name}} ({{$sport->number_of_players}}v{{$sport->number_of_players}})</option>
                         @endforeach
@@ -96,21 +73,7 @@
                         <p style="color:red;">{{$message}}</p>
                     @enderror
                 </div>
-{{--                <label class="item" for="tournament-sport">Sport</label>--}}
-{{--                <select class="item" id="tournament-sport" name="tournament_sport">--}}
-{{--                    @foreach($sports as $sport)--}}
-{{--                    <option value="{{$sport->name}}">{{$sport->name}} ({{$sport->number_of_players}}v{{$sport->number_of_players}})</option>--}}
-{{--                    @endforeach--}}
-{{--                </select>--}}
 
-                {{-- <span class="item" id="label-for-switch"> Individual participants</span>
-                <label class="switch item" for="tournament-for-teams">
-
-                        <input type="checkbox" name="tournament-for-teams" id="tournament-for-teams" onclick="window.switchButtonHandler()">
-                        <span class="slider round" id="switch-round-button"></span>
-                </label> --}}
-
-{{--                <button type="submit" id="submit-button">Create Tournament</button>--}}
                 <input type="submit" id="submit-button" value="Create tournament">
             </form>
 
