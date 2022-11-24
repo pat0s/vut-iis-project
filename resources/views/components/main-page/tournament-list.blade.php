b0f17a2f67c67dec80f32d7bd0913f48fbf1ca9d
Anton Medvedev <amedvede@redhat.com>
2022-11-24 20:48:44 +0100

    feat: number of players of sports

--- resources/views/components/main-page/tournament-list.blade.php
+++ resources/views/components/main-page/tournament-list.blade.php
@@ -11,7 +11,7 @@
 
                 :tournamentName="$tournament->tournament_name"
 
-                :sport="$tournament->sport->name"
+                :sport="$tournament->sport->name.' ('.$tournament->sport->number_of_players.' vs '.$tournament->sport->number_of_players.')'"
                 :numberOfParticipants="$tournament->number_of_participants"
                 :dateOfStart="date('d-m-Y', strtotime($tournament->start_date))"
             />
--- resources/views/components/tournament-page/tournaments-list.blade.php
+++ resources/views/components/tournament-page/tournaments-list.blade.php
@@ -6,7 +6,7 @@
             <x-tournament-page.tournaments-list-item
                 :tournamentID="$tournament->tournament_id"
                 :tournamentName="$tournament->tournament_name"
-                :sport="$tournament->sport->name"
+                :sport="$tournament->sport->name.' ('.$tournament->sport->number_of_players.' vs '.$tournament->sport->number_of_players.')'"
                 :numberOfParticipants="$tournament->number_of_participants"
                 :dateOfStart="date('d-m-Y', strtotime($tournament->start_date))"
             />
