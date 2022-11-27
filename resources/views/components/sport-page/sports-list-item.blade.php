f07f89aa199f5360b448e9fd3eccb2eda467a2be
Patrik Sehnoutek <patriksehnoutek@gmail.com>
2022-11-27 19:30:22 +0100

    refactor: remove redundant code

--- resources/views/components/sport-page/sports-list-item.blade.php
+++ resources/views/components/sport-page/sports-list-item.blade.php
@@ -1,12 +1,5 @@
-@props([
-    'sportID' => '1',
-    'sportName' => 'Sport name',
-    'numberOfPlayers' => '1',
-])
-
-
 <li class="sport-list-item">
-        <p>ID: &nbsp;&nbsp;{{$sportID}}</p>
-        <p>{{$sportName}}</p>
-        <p>{{$numberOfPlayers}}</p>
+    <p>ID: &nbsp;&nbsp;{{$sportID}}</p>
+    <p>{{$sportName}}</p>
+    <p>{{$numberOfPlayers}}</p>
 </li>
